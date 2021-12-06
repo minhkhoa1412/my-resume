de2134107bf5a6433f1ffdf9d94b8f07db60f302
khoatm <khoatm@payme.vn>
2021-12-06 14:12:15 +0700

    web demo

---
+++ App.web.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import {
+  StyleSheet, useColorScheme, View
+} from 'react-native'
+import 'react-native-reanimated'
+import { HomeScreen } from './src/screen/HomeScreen/HomeScreen'
+import { SafeAreaProvider } from 'react-native-safe-area-context/src/SafeAreaContext'
+import { NavigationContainer } from '@react-navigation/native'
+import { CardStyleInterpolators, createStackNavigator, TransitionSpecs } from '@react-navigation/stack'
+import { ProjectScreen } from './src/screen/ProjectScreen/ProjectScreen'
+
+const Stack = createStackNavigator()
+
+const App = () => {
+  const isDarkMode = useColorScheme() === 'dark'
+
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
+
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={{
+              cardStyleInterpolator: CardStyleInterpolators.forNoAnimation
+            }}
+          />
+          <Stack.Screen
+            name="Project"
+            component={ProjectScreen}
+            options={{
+              cardStyleInterpolator: CardStyleInterpolators.forNoAnimation
+            }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
+}
+
+const s = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white'
+  }
+})
+
+export default App
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin'
-  ],
+module.exports = (api) =>  {
+  api.cache(false)
+
+  return {
+    presets: ['module:metro-react-native-babel-preset'],
+    plugins: [
+      '@babel/plugin-transform-flow-strip-types',
+      '@babel/plugin-proposal-class-properties',
+      'react-native-reanimated/plugin'
+    ],
+  }
 }
+
